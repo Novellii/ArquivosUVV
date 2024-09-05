5f26e64d19b6f447e76532aa5b2b05cff82c333d
Pedro Henrique Novelli Soares <125471109+Novellii@users.noreply.github.com>
2024-09-05 07:52:36 -0300

    Add files via upload

---
+++ ProgramaçãoObjetos2/tapoxa/Classes/Capivara.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tapoxa.Classes
+{
+    internal class Capivaras
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public float peso { get; set; }
+        public Capivaras(string nome, int idade, float peso)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+        }
+    }
+}
---
+++ ProgramaçãoObjetos2/tapoxa/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using tapoxa.Classes;
+
+namespace tapoxa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<Capivaras> listCapivaras = new BindingList<Capivaras>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridCapivaras.ItemsSource = listCapivaras;
+        }
+
+        private void ClicarBotão(object sender, RoutedEventArgs e)
+        {
+            Capivaras cp = new Capivaras(Nome.Text, int.Parse(Idade.Text), float.Parse(Peso.Text));
+            listCapivaras.Add(cp);
+        }
+    }
+}
