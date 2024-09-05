c8ea2d86d2b197b3a9d8eae87d65e5539c232922
Pedro Henrique Novelli Soares <125471109+Novellii@users.noreply.github.com>
2024-09-05 07:56:47 -0300

    Add files via upload

---
+++ ProgramaçãoObjetos2/NomeCompleto/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NomeCompleto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxName.Text == String.Empty || TextBoxSobrenome.Text == String.Empty) {
+                ButtonNS.Text = "SEM NOME";
+            }
+            else
+            {
+                ButtonNS.Text = $"{TextBoxName.Text} {TextBoxSobrenome.Text}";
+            }
+        }
+    }
+}
