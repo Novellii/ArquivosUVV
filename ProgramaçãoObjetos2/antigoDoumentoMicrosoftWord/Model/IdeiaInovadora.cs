dde0b1ffe5dbe8a96f3f5388da69188f91c37b38
Pedro Henrique Novelli Soares <125471109+Novellii@users.noreply.github.com>
2024-09-05 08:50:42 -0300

    Add files via upload

---
+++ ProgramaçãoObjetos2/antigoDoumentoMicrosoftWord/Model/IdeiaInovadora.cs
@@ -0,0 +1,29 @@
+﻿using antigoDoumentoMicrosoftWord.Persistencs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antigoDoumentoMicrosoftWord.Model
+{
+    internal class IdeiaInovadora
+    {
+        public string Area { get; set; }
+        public string Ideia { get; set; }
+        public float Custo { get; set; }
+
+        public IdeiaInovadora(string area, string ideia, float custo)
+        {
+            Area = area;
+            Ideia = ideia;
+            Custo = custo;
+        }
+
+        public Boolean CadastrarIdeiaInovacao(IdeiaInovadora i) 
+        {
+            BD.SalvarBD(i);
+            return true; 
+        }
+    }
+}
---
+++ ProgramaçãoObjetos2/antigoDoumentoMicrosoftWord/Persistencs/BD.cs
@@ -0,0 +1,22 @@
+﻿using antigoDoumentoMicrosoftWord.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antigoDoumentoMicrosoftWord.Persistencs
+{
+    internal class BD
+    {
+        public static List<IdeiaInovadora> mybd = new();
+
+        public static void SalvarBD(IdeiaInovadora i) { 
+            mybd.Add(i); 
+        }
+
+        public static List<IdeiaInovadora> RetomarBD() {
+            return mybd;    
+        }
+    }
+}
